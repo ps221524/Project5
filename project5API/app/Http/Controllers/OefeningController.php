c7ecc1905bae57cbcc374d4fd758372f5e4a2635
Dani <196391@edu.summacollege.nl>
2022-06-17 11:42:34 +0200

    Update op net

---
+++ project5API/app/Http/Controllers/OefeningController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Oefening;
+use Illuminate\Http\Request;
+
+class OefeningController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Oefening  $oefening
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Oefening $oefening)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Oefening  $oefening
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Oefening $oefening)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Oefening  $oefening
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Oefening $oefening)
+    {
+        //
+    }
+}
