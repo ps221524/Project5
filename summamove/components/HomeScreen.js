f34e8563c589e08b6da30d79efbd0d12c84df8ad
ps219846 <ps219846@edu.summacollege.nl>
2022-06-23 15:43:46 +0200

    Api ophalen

--- summamove/components/HomeScreen.js
+++ summamove/components/HomeScreen.js
@@ -1,13 +1,54 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import React, { Component, useEffect, useState } from 'react';
+import {ActivityIndicator, FlatList, Text, View, TouchableOpacity, Button, StyleSheet} from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 
-import { StyleSheet, Text, View } from 'react-native';
 
+function Home({navigation}) {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
 
-const Home = () => {
-  return (
-    <View style={styles.container}>
-    <Text>Home page</Text>
-    </View>
-  );
+  const getApi = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/api/exercise');
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getApi();
+  }, []);
+    return (
+        <View>
+          {isLoading ? <ActivityIndicator/> : (
+              <FlatList
+                  data={data}
+                  keyExtractor={({ id }, index) => id}
+                  renderItem={({ item }) => (
+                      <TouchableOpacity onPress={() => navigation.navigate('Details', {
+                          id: item.id,
+                          name: item.name,
+                      })}>
+                        <View style={styles.itemborder}>
+                           
+                            <View>
+
+                            </View>
+                          <Text style={styles.item} >{item.name}</Text>
+                        </View>
+                      </TouchableOpacity>
+                  )}
+              />
+          )}
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -17,6 +58,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+item: {
+  fontSize: 30,
+  marginLeft: 15,
+  marginRight: 15,
+  width: 100,
+  textAlign:'center',
+  height: 50,
+},
 });
 
-export default Home;
+export default Home
